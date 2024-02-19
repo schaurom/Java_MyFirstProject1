400f407908b6e3e6f3a307b4b400e0ae41ce0129
schaurom <118014904+schaurom@users.noreply.github.com>
2024-02-19 20:24:57 +0100

    .

---
+++ src/IntegerArithmetic.java
@@ -0,0 +1,29 @@
+public class IntegerArithmetic {
+
+    public static void main(String[] args) {
+
+        int addition = 42 + 6;
+        System.out.println(addition);
+
+        int subtraction = 42 - 7;
+        System.out.println(subtraction);
+
+        int negativeNumbers = 5 - 43;
+        System.out.println(negativeNumbers);
+
+        int multiplication = 42 * 4;
+        System.out.println(multiplication);
+
+        int division = 42 / 2;
+        System.out.println(division);
+
+        int unexpectedDivision = 5 / 2;
+        System.out.println(unexpectedDivision);
+
+        int operatorPrecedence = 5 + 2 * 10;
+        System.out.println(operatorPrecedence);
+
+        int parantheses = (5 + 2) * 10;
+        System.out.println(parantheses);
+    }
+}
--- src/LocalVariableTypeInference.java
+++ src/LocalVariableTypeInference.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+
 public class LocalVariableTypeInference {
 
     public static void main(String[] args) {
+
+        var myNumber = 42;
+        myNumber = 11;
+
+        var myString = "java";
+
+        System.out.println(myNumber + " " + myString);
+
 
     }
 }
